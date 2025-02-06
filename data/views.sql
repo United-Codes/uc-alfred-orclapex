bc0246ac641a246121e727eda14f5c07834e36e6
phartenfeller <pharti@hotmail.de>
2025-02-06 15:59:35 +0100

    add mail views manually and 24.2 views

--- data/views.sql
+++ data/views.sql
@@ -8,6 +8,16 @@ select apex_view_name as "name"
      , comments as "description"
      , parent_view as "parentView"
   from apex_dictionary
- where column_id = 0;
+ where column_id = 0
+ union all
+select 'APEX_MAIL_LOG' as "name"
+     , 'Log of APEX mails' as "description"
+     , null as "parentView"
+  from dual
+ union all
+select 'APEX_MAIL_QUEUE' as "name"
+     , 'Mail queue' as "description"
+     , null as "parentView"
+  from dual;
 
 SPOOL OFF
